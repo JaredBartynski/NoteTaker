3035adf0a79d4a8a68ce8846cd03c610ae44400e
JaredBartynski <70081376+JaredBartynski@users.noreply.github.com>
2020-10-02 21:34:21 -0400

    added parsing

--- Develop/server.js
+++ Develop/server.js
@@ -4,3 +4,13 @@ const path = require("path");
 const fs = require("fs");
 const { json } = require("express");
 const { publicDecrypt } = require("crypto");
+
+//Sets up Express App
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+//Handles data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
